0ae820e99163e09a204e6a40592201c73f473aa1
JessicaSaantos <jessicaferre998@gmail.com>
2022-10-01 21:43:42 -0300

    desenvolvendo estrutura de código das classes

--- sacola-api/src/main/java/me/dio/sacola/model/Restaurante.java
+++ sacola-api/src/main/java/me/dio/sacola/model/Restaurante.java
@@ -1,4 +1,28 @@
 package me.dio.sacola.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor //Criar construtor com tds atributos
+@Builder // Criar objeto simples
+@Data // Criar Getter and Setters
+@Entity //converter a class em tabela no banco de dados
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor // construtor vazio
+
 public class Restaurante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Produto> cardapio;
+    @Embedded
+    private Endereco endereco;
 }
